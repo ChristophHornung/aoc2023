52ecbf507bb9898185b94c6edb7721ac564f4f80
Christoph Hornung <christoph.hornung@this.de>
2023-12-02 11:03:18 +0100

    Day2

---
+++ Day2/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Day2;
+
+using Base;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		new Day2().Run(false);
+	}
+}
+
+internal class Day2 : DayX
+{
+	public void Run(bool isPart2)
+	{
+		using StreamReader input = this.GetInput();
+		string? line = input.ReadLine();
+		List<Game> games = [];
+		while (line != null)
+		{
+			games.Add(this.ParseLine(line));
+			line = input.ReadLine();
+		}
+
+
+		this.ReportResult(games.Where(this.IsGamePossible).Sum(r => r.Id));
+		this.ReportResult(games.Sum(this.GetMinimalPower));
+	}
+
+	private bool IsGamePossible(Game g)
+	{
+		return g.Reveals.All(r => r is {Red: <= 12, Green: <= 13, Blue: <= 14});
+	}
+
+	private long GetMinimalPower(Game g)
+	{
+		return g.Reveals.Max(r => r.Blue) * g.Reveals.Max(r => r.Green) * g.Reveals.Max(r => r.Red);
+	}
+
+	private Game ParseLine(string line)
+	{
+		string[] gameSplit = line.Split(':');
+		int id = int.Parse(gameSplit[0][5..]);
+		string[] revealsSplit = gameSplit[1].Split(';');
+		List<Reveal> reveals = [];
+		foreach (string revealString in revealsSplit)
+		{
+			int red = 0;
+			int green = 0;
+			int blue = 0;
+			string[] revealSplit = revealString.Split(',', StringSplitOptions.TrimEntries);
+			foreach (string item in revealSplit)
+			{
+				string[] itemSplit = item.Split(' ');
+				int count = int.Parse(itemSplit[0]);
+				string color = itemSplit[1];
+				if (color == "red")
+				{
+					red = count;
+				}
+				else if (color == "green")
+				{
+					green = count;
+				}
+				else if (color == "blue")
+				{
+					blue = count;
+				}
+			}
+
+			reveals.Add(new Reveal(red, green, blue));
+		}
+
+		return new Game(id, reveals);
+	}
+}
+
+internal record struct Game(int Id, List<Reveal> Reveals);
+
+internal record struct Reveal(int Red, int Green, int Blue);
