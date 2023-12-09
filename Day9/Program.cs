04b3d508c926fbef43930edcefe8e1d3ac79565b
Christoph Hornung <christoph.hornung@this.de>
2023-12-09 10:04:22 +0100

    Day9-2

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -8,6 +8,7 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		new Day9().Run(false);
+		new Day9().Run(true);
 	}
 
 	internal class Day9 : DayX
@@ -22,19 +23,23 @@ internal class Program
 			{
 				long[] seeds = new string(line.Where(c => char.IsNumber(c) || c == ' ' || c == '-').ToArray())
 					.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+				if (isPart2)
+				{
+					seeds = seeds.Reverse().ToArray();
+				}
+
 				long result = seeds[^1];
-				var i = 1;
+				int i = 1;
 				long diff = seeds[^i] - seeds[^(i + 1)];
 				while (i < seeds.Length - 1)
 				{
-					Console.WriteLine(diff);
 					result += diff;
 					i++;
 					diff = seeds[^1];
 					int add = -1;
-					for (var j = 1; j <= i; j++)
+					for (int j = 1; j <= i; j++)
 					{
-						var leftTurnCombinations = (long)SpecialFunctions.Binomial(i, j);
+						long leftTurnCombinations = (long)SpecialFunctions.Binomial(i, j);
 						diff += add * leftTurnCombinations * seeds[^(j + 1)];
 						add *= -1;
 					}
